abd6a771ed6162400b0acdb8773cc90535cd68e5
splitice <mat999@gmail.com>
2014-02-24 19:36:24 +1100

    abstracted a more general validation exception

--- src/Radical/Exceptions/ValidationException.php
+++ src/Radical/Exceptions/ValidationException.php
@@ -2,20 +2,5 @@
 namespace Radical\Exceptions;
 
 class ValidationException extends \Exception {
-	protected $field;
-	function __construct($field = null){
-		$this->field = $field;
-		$message = 'A validation Exception occured';
-		if($field){
-			$message .= ' with '.$field;
-		}
-		parent::__construct($message);
-	}
 	
-	/**
-	 * @return the $field
-	 */
-	public function getField() {
-		return $this->field;
-	}
 }
